f13f2f77837d8f0d6a5d78824ff2cc2f24fa0d7b
saber71 <1533357473@qq.com>
2024-10-01 19:21:26 +0800

    添加了flatCase、camelCase、underscoreCase和kebabCase字符串转换方法

--- utils-type/src/main/kotlin/heraclius/utils_type/StringUtils.kt
+++ utils-type/src/main/kotlin/heraclius/utils_type/StringUtils.kt
@@ -2,14 +2,42 @@ package heraclius.utils_type
 
 object StringUtils {
     fun firstUpper(string: String): String {
+        if (string.isEmpty()) return string
         return string.first().uppercase() + string.substring(1)
     }
 
     fun firstLower(string: String): String {
+        if (string.isEmpty()) return string
         return string.first().lowercase() + string.substring(1)
     }
 
     fun toList(string: String): List<String> {
         return string.toList().map { it.toString() }
+    }
+
+    fun flatCase(string: String): String {
+        return string.lowercase().replace("-", "").replace("_", "")
+    }
+
+    fun camelCase(string: String): String {
+        return firstLower(string.split(Regex("[-_]")).filter { it.isNotEmpty() }.joinToString("") { firstUpper(it) })
+    }
+
+    fun underscoreCase(string: String): String {
+        return string.split("-")
+            .joinToString("_")
+            .map { if (it.isUpperCase()) "_" + it.lowercase() else it }
+            .joinToString("")
+            .replace(Regex("__+"), "_")
+            .replace(Regex("(_+$)|(^_+)"), "")
+    }
+
+    fun kebabCase(string: String): String {
+        return string.split("_")
+            .joinToString("-")
+            .map { if (it.isUpperCase()) "-" + it.lowercase() else it }
+            .joinToString("")
+            .replace(Regex("--+"), "-")
+            .replace(Regex("(-+$)|(^-+)"), "")
     }
 }
--- utils-type/src/test/kotlin/heraclius/utils_type/StringUtilsTest.kt
+++ utils-type/src/test/kotlin/heraclius/utils_type/StringUtilsTest.kt
@@ -15,4 +15,32 @@ class StringUtilsTest {
         assertEquals(StringUtils.firstLower("Abc"), "abc")
         assertEquals(StringUtils.firstLower("A"), "a")
     }
+
+    @Test
+    fun flatCase() {
+        assertEquals(StringUtils.flatCase("HelloWorld"), "helloworld")
+        assertEquals(StringUtils.flatCase("-HelloWorld"), "helloworld")
+        assertEquals(StringUtils.flatCase("-Hello_-World_"), "helloworld")
+    }
+
+    @Test
+    fun kebabCase() {
+        assertEquals(StringUtils.kebabCase("HelloWorld"), "hello-world")
+        assertEquals(StringUtils.kebabCase("-HelloWorld"), "hello-world")
+        assertEquals(StringUtils.kebabCase("-Hello_-World_"), "hello-world")
+    }
+
+    @Test
+    fun underscoreCase() {
+        assertEquals(StringUtils.underscoreCase("HelloWorld"), "hello_world")
+        assertEquals(StringUtils.underscoreCase("-HelloWorld"), "hello_world")
+        assertEquals(StringUtils.underscoreCase("-Hello_-World_"), "hello_world")
+    }
+
+    @Test
+    fun camelCase() {
+        assertEquals(StringUtils.camelCase("HelloWorld"), "helloWorld")
+        assertEquals(StringUtils.camelCase("-HelloWorld"), "helloWorld")
+        assertEquals(StringUtils.camelCase("-Hello_-World_"), "helloWorld")
+    }
 }
