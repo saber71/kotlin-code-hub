2410608c573ebe5bb9b08caf6308b108fccbb73e
saber71 <1533357473@qq.com>
2024-10-02 13:57:36 +0800

    改进`ClassReflection`类并添加新功能
    - 为`setProperty`函数添加不抛出异常的选项
    - 实现`setProperties`函数的异常控制
    - 新增`assign`函数用于对象属性赋值
    - 优化`Main.kt`中的示例代码

--- utils-reflection/src/main/kotlin/heraclius/utils_reflection/ClassReflection.kt
+++ utils-reflection/src/main/kotlin/heraclius/utils_reflection/ClassReflection.kt
@@ -4,6 +4,8 @@ import heraclius.utils_dict.Dict
 import heraclius.utils_type.TypeUtils
 import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 import kotlin.reflect.jvm.javaType
 
 object ClassReflection {
@@ -12,26 +14,42 @@ object ClassReflection {
         return cls.java.getConstructor(*args.map { TypeUtils.javaClass(it) }.toTypedArray()).newInstance(*args)
     }
 
-    fun <T : Any> setProperty(inst: T, name: String, value: Any) {
+    fun <T : Any> setProperty(inst: T, name: String, value: Any, throws: Boolean = false): Boolean {
         val kClass = inst::class
-        val member = kClass.members.find { it.name == name }
-        if (member == null) throw IllegalArgumentException("No such property: $name")
-        if (member is KMutableProperty)
+        val member = kClass.memberProperties.find { it.name == name }
+        if (member == null) {
+            if (throws) throw NoSuchFieldException("No such field $name")
+            return false
+        }
+        if (member is KMutableProperty<*>) {
+            member.isAccessible = true
             member.setter.call(inst, TypeUtils.to(member.returnType.javaType as Class<*>, value))
-        else
-            throw IllegalArgumentException("${member.name} is not a mutable property")
+            return true
+        } else if (throws)
+            throw IllegalArgumentException("Property $name is not mutable")
+        return false
     }
 
-    fun <T : Any> setProperties(inst: T, keyValue: Map<Any, Any>): T {
+    fun <T : Any> setProperties(inst: T, keyValue: Map<Any, Any>, throws: Boolean = false): T {
         for (entry in keyValue) {
             val key = TypeUtils.to(String::class.java, entry.key)
-            setProperty(inst, key, entry.value)
+            setProperty(inst, key, entry.value, throws)
         }
         return inst
     }
 
-    fun <T : Any> setProperty(inst: T, dict: Dict): T {
+    fun <T : Any> setProperties(inst: T, dict: Dict, throws: Boolean = false): T {
         @Suppress("UNCHECKED_CAST")
-        return setProperties(inst, dict.toMap() as Map<Any, Any>)
+        return setProperties(inst, dict.toMap() as Map<Any, Any>, throws)
+    }
+
+    fun <T : Any> assign(dst: T, vararg src: Any, throws: Boolean = false): T {
+        for (item in src) {
+            val srcKClass = src::class
+            for (property in srcKClass.memberProperties) {
+                setProperty(dst, property.name, property.getter.call(src) as Any, throws)
+            }
+        }
+        return dst
     }
 }
